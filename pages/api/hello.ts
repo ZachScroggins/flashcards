82103973f2c5358b23f27d52c6363069e6579125
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2021-05-19 17:48:29 -0500

    strings

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,24 +5,24 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
 
-  console.log('NEXT_PUBLIC_VERCEL_ENV: ', +process.env.NEXT_PUBLIC_VERCEL_ENV);
-  console.log('NEXT_PUBLIC_VERCEL_URL: ', +process.env.NEXT_PUBLIC_VERCEL_URL);
-
-  console.log('Database URL: ', +process.env.DATABASE_URL);
-
-  console.log('NEXTAUTH_URL: ', +process.env.NEXTAUTH_URL);
-  console.log('NEXT_AUTH_SECRET: ', +process.env.NEXT_AUTH_SECRET);
-  console.log('NEXT_AUTH_JWT_SECRET: ', +process.env.NEXT_AUTH_JWT_SECRET);
+  console.log(`NEXT_PUBLIC_VERCEL_ENV: ${process.env.NEXT_PUBLIC_VERCEL_ENV}`);
   console.log(
-    'JWT_SIGNING_PRIVATE_KEY: ',
-    +process.env.JWT_SIGNING_PRIVATE_KEY
+    `NEXT_PUBLIC_VERCEL_URL: ${process.env.NEXT_PUBLIC_VERCEL_URL}\n`
   );
-  console.log('GITHUB_CLIENT_ID: ', +process.env.GITHUB_CLIENT_ID);
-  console.log('GITHUB_CLIENT_SECRET: ', +process.env.GITHUB_CLIENT_SECRET);
+
+  console.log(`Database URL: ${process.env.DATABASE_URL}\n`);
+
+  console.log(`NEXTAUTH_URL: ${process.env.NEXTAUTH_URL}\n`);
+  console.log(`NEXT_AUTH_SECRET: ${process.env.NEXT_AUTH_SECRET}`);
+  console.log(`NEXT_AUTH_JWT_SECRET: ${process.env.NEXT_AUTH_JWT_SECRET}`);
+  console.log(
+    `JWT_SIGNING_PRIVATE_KEY: ${process.env.JWT_SIGNING_PRIVATE_KEY}`
+  );
+  console.log(`GITHUB_CLIENT_ID: ${process.env.GITHUB_CLIENT_ID}`);
+  console.log(`GITHUB_CLIENT_SECRET: ${process.env.GITHUB_CLIENT_SECRET}\n`);
 
   console.log(
-    'NEXT_PUBLIC_GRAPHQL_API: ',
-    +process.env.NEXT_PUBLIC_GRAPHQL_API
+    `NEXT_PUBLIC_GRAPHQL_API: ${process.env.NEXT_PUBLIC_GRAPHQL_API}`
   );
 
   switch (method) {
